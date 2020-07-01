ac68ce3755c177ad610177c32f842c599d54ea54
Slimchux1 <38258308+Slimchux1@users.noreply.github.com>
2020-07-02 00:52:00 +0100

    Add files via upload

---
+++ precache-manifest.e87b1316ec3076cd589e4f00a75b1389.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2009682c83360c977222ab67b683d0",
+    "url": "/n-power-replica/index.html"
+  },
+  {
+    "revision": "e4529b0c4ba8e260003b",
+    "url": "/n-power-replica/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b98813dbf5d56fb5942",
+    "url": "/n-power-replica/static/css/main.1da35504.chunk.css"
+  },
+  {
+    "revision": "e4529b0c4ba8e260003b",
+    "url": "/n-power-replica/static/js/2.8cc0cf9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/n-power-replica/static/js/2.8cc0cf9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b98813dbf5d56fb5942",
+    "url": "/n-power-replica/static/js/main.ad4500c6.chunk.js"
+  },
+  {
+    "revision": "07467fa6871f08f9b736",
+    "url": "/n-power-replica/static/js/runtime-main.dea9258f.js"
+  }
+]);
